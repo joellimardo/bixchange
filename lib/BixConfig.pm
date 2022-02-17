3b770a7cfb43f120b9b1516da649a2754ba13adb
Joel Limardo <joellimardo@hotmail.com>
2022-02-17 17:45:35 -0600

    updated version

--- lib/BixConfig.pm
+++ lib/BixConfig.pm
@@ -108,7 +108,7 @@ $BixChange::bix{VERSION_NUMBER} = 1.8;
 
 #VERSION_PATCH, a letter designation indicating a patch (a,b,c...etc). So a
 #real version name for a given release could be 1.8b.
-$BixChange::bix{VERSION_PATCH} = q|c|;
+$BixChange::bix{VERSION_PATCH} = q|f|;
 
 #VERSION, combination of VERSION_NUMBER and VERSION_PATCH. Use as needed.
 $BixChange::bix{'VERSION'} = $BixChange::bix{'VERSION_NUMBER'} . $BixChange::bix{'VERSION_PATCH'};
